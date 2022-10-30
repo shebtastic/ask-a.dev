830644db49bcac51e31925700ade74b8d612b4a1
Sheb <michael@sheb.rocks>
2022-10-30 21:23:34 +0100

    add faked questions overview

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,47 @@
 import Head from 'next/head'
 
+const questions = [
+  {
+    id: 'EF370F87-338F-47F2-AD2C-7CB23F3B9669',
+    question: 'test?',
+  },
+  {
+    id: 'B8AE42B9-5C0E-4336-A998-614A6A7249B9',
+    question: 'y tho?',
+  },
+  {
+    id: 'E3EB7364-CD78-4CC1-B616-C5CF588E3304',
+    question: 'Is this the real life?',
+  },
+  {
+    id: 'A997467E-E083-482C-B25C-B72B603CECC4',
+    question: 'Is this just fantasy?',
+  },
+]
+
 function Home() {
   return (
     <>
       <Head>
-        <title>Welcome!</title>
+        <title>Ask away!</title>
       </Head>
-      <main>Hello!</main>
+      <main>
+        <section>
+          <h1>Ask a dev!</h1>
+          <p>
+            Feel free to browse or ask any question while your identity stays
+            stealthy and hidden.
+          </p>
+          <aside>🥷Like a freakin' ninja!</aside>
+        </section>
+        <section>
+          <ul>
+            {questions.map(({ id, question }) => (
+              <li key={id}>{question}</li>
+            ))}
+          </ul>
+        </section>
+      </main>
     </>
   )
 }
